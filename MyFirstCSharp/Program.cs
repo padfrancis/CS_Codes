8ca1b2ad6382a3c3a6153e1d8388e6f88fb64442
padfrancis <francispadron@spcis.edu.ph>
2023-09-09 16:01:21 +0800

    First C# Code

---
+++ MyFirstCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
