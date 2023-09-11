63f1c184d220c3ac78498a015254f283a8f5c06f
padfrancis <francispadron@spcis.edu.ph>
2023-09-11 09:56:22 +0800

    Added basic Operations C#

---
+++ MyFirstCSharp/BasicOperations.cs
@@ -0,0 +1,59 @@
+﻿/* 
+//First Lesson
+int num1 = 10;
+int num2 = 2;
+Console.WriteLine(num1 + num2);
+
+string fname = "Francis";
+int sold = 10;
+Console.WriteLine(fname + " sold " + (sold + 10) + " coffees.");
+*/
+/*
+//Second Lesson
+int sum = 3 + 19;
+int diff = sum - 10;
+int prod = diff * sum;
+int quotient = prod / diff;
+decimal dquo = quotient / diff;
+int first = 7;
+int second = 5;
+decimal quotient1 = (decimal)first / (decimal)second;
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+Console.WriteLine(quotient1);
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + diff);
+Console.WriteLine("Product: " + prod);
+Console.WriteLine("Quotient: " + quotient);
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int result = 3 + 1 * 5 / 2;
+Console.WriteLine(result);
+*/
+/*
+//Third Lesson
+int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;        // value is now 10.
+
+int value1 = 0;     // value is now 0.
+value1 = value1 + 1; // value is now 1.
+value1++;           // value is now 2.
+
+int value2 = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine("Second: " + value++);
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+*/
+/*
+//Farenheit
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
+*/
